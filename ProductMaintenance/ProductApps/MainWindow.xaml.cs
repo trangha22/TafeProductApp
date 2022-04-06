0f097e154c7a3a1f7a590b45f28bd8d1e8f710e7
trangha22 <trangdn2106@gmail.com>
2022-04-06 13:45:32 +0930

    Commit the Product Maintenance program to the local repository

---
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const decimal DELIVERYCHARGE = 25.00M;
+        private const decimal GSTRATE = 00.1M;
+        Product cProduct;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
+                cProduct.calTotalPayment();
+                totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+                decimal totalCharge = cProduct.TotalPayment + DELIVERYCHARGE;
+                totalChargeTextBlock.Text = totalCharge.ToString();
+                decimal totalwithwrap = totalCharge + 5;
+                totalwithwraptextblock.Text = totalwithwrap.ToString();
+                decimal totalwithGST = totalwithwrap * GSTRATE + totalwithwrap;
+                totalGSTBlock.Text = totalwithGST.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Enter data again", "Data Entry Error");
+            }
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            productTextBox.Text = "";
+            priceTextBox.Text = "";
+            quantityTextBox.Text = "";
+            totalPaymentTextBlock.Text = "";
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
